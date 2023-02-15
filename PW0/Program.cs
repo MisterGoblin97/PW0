b8ced753a07430212ef7c62fbec04ea07579cd7e
MisterGoblin96 <krestinenko.matvey@gmail.com>
2023-02-15 13:08:54 +0300

    Добавьте файлы проекта.

---
+++ PW0/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program {
+
+    static decimal CalculateBMI(decimal height, decimal weight) {
+        return decimal.Round(((weight/(height*height))*10000), 2);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(CalculateBMI(170, 65));
+    }
+}
